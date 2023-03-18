dceb701f6e2a35d491c49871cbfd875e5acb8877
lhkhiem28 <lhkhiem28@gmail.com>
2023-03-18 07:10:57 +0000

    initial

--- warmup/main/train.py
+++ warmup/main/train.py
@@ -1,13 +1,16 @@
 import os, sys
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(__dir__), sys.path.append(os.path.abspath(os.path.join(__dir__, "..")))
 from libs import *
 
 from data import ImageDataset
+from models import ResNet18
 from engines import train_fn
 
 train_loaders = {
     "train":torch.utils.data.DataLoader(
         ImageDataset(
-            data_dir = "../datasets/P-ACS/{}/".format("ACS"), 
+            data_dir = "../../datasets/P-ACS/{}/".format("ACS"), 
             augment = True, 
         ), 
         num_workers = 8, batch_size = 16, 
@@ -15,16 +18,15 @@ train_loaders = {
     ), 
     "val":torch.utils.data.DataLoader(
         ImageDataset(
-            data_dir = "../datasets/P-ACS/{}/".format("P"), 
+            data_dir = "../../datasets/P-ACS/{}/".format("P"), 
             augment = False, 
         ), 
         num_workers = 8, batch_size = 16, 
         shuffle = False, 
     ), 
 }
-model = torchvision.models.resnet18(pretrained = True)
-model.fc = nn.Linear(
-    model.fc.in_features, 7, 
+model = ResNet18(
+    num_classes = 7, 
 )
 optimizer = optim.SGD(
     model.parameters(), weight_decay = 5e-4, 
@@ -35,7 +37,7 @@ lr_scheduler = optim.lr_scheduler.StepLR(
     step_size = 20, gamma = 0.1, 
 )
 
-save_ckp_dir = "../ckps/P-ACS/{}".format("P")
+save_ckp_dir = "../../ckps/P-ACS/{}".format("P")
 if not os.path.exists(save_ckp_dir):
     os.makedirs(save_ckp_dir)
 train_fn(
